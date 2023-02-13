61092e139919d56283829dc24f904d1b6c0a4939
gvsco46 <72887952+gvsco46@users.noreply.github.com>
2023-02-13 00:00:49 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,37 @@
+pokemon = {
+    'Pikachu' : {
+        'status' : ['HP: 300', 'Attack: 600', 'Defense: 500', 'Speed: 300', 'Total: 1.700'],
+        'skills' : ['Thunder shock', 'Halter'],
+        'type' : ['#025', 'Electric', 'images/pikachu.png', '#FCC719'],   
+    },
+
+    'Bulbasaur' : {
+        'status' : ['HP: 320', 'Attack: 310', 'Defense: 400', 'Speed: 200', 'Total: 1.230'],
+        'skills' : ['Razor leaves', 'Sunbeam'],
+        'type' : ['#001', 'Grass', 'images/bulbasaur.png', '#49D0B0'],   
+    },
+
+    'Charmander' : {
+        'status' : ['HP: 200', 'Attack: 300', 'Defense: 400', 'Speed: 320', 'Total: 1.220'],
+        'skills' : ['Scratch', 'Reprisal'],
+        'type' : ['#004', 'Fire', 'images/charmander.png', '#ED8A8B' ],   
+    },
+
+    'Gyarados' : {
+        'status' : ['HP: 600', 'Attack: 897', 'Defense: 310', 'Speed: 600', 'Total: 2.407'],
+        'skills' : ['Dragon Rage', 'Aqua Rush'],
+        'type' : ['#452', 'Water', 'images/gyarados.png', '#76BEFE'],   
+    },
+
+    'Gengar' : {
+        'status' : ['HP: 451', 'Attack: 845', 'Defense: 810', 'Speed: 022', 'Total: 2.128'],
+        'skills' : ['Life Shaker', 'Hypnoblast'],
+        'type' : ['#898', 'Dark', 'images/gengar.png', '#BA68C8'],   
+    },
+
+    'Dragonite' : {
+        'status' : ['HP: 787', 'Attack: 822', 'Defense: 151', 'Speed: 999', 'Total: 2.759'],
+        'skills' : ['Max Wind', 'Mach Press'],
+        'type' : ['#111', 'Dragon', 'images/dragonite.png', '#C29791'],   
+    }
+}
